bad5bcbabc7e936f13381d5b182107c0864f3147
Phan Hoang Trong Tin <Chouphan1207@gmail.com>
2026-04-21 19:48:56 +0700

    feat: update theme styles and enhance ThemeSwitcher component

--- src/widgets/Header/ui/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/widgets/Header/ui/ThemeSwitcher/ThemeSwitcher.tsx
@@ -4,11 +4,20 @@ import { useTheme } from "@/shared/config";
 import { AppIcon, Button } from "@/shared/ui";
 
 export const ThemeSwitcher = () => {
-  const { toggleTheme } = useTheme();
+  const { theme, toggleTheme } = useTheme();
+
+  // Define a color for each theme
+  const themeColors: Record<string, string> = {
+    "light-theme": "#79B4B7", // Amber
+    "dark-theme": "#1B3C53", // Sky blue
+    // Add more themes and colors as needed
+  };
+
+  const iconColor = themeColors[theme]; // Default gray
 
   return (
     <Button theme="tertiary" onClick={toggleTheme}>
-      <AppIcon Icon={CircleIcon} filled />
+      <CircleIcon style={{ fill: iconColor, width: 24, height: 24 }} />
     </Button>
   );
 };
