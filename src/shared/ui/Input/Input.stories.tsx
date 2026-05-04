a52a2f1155b8b7686db87f3e9a2e927d83dcd24f
Phan Hoang Trong Tin <Chouphan1207@gmail.com>
2026-05-04 21:34:54 +0700

    input fixing #2

--- src/shared/ui/Input/Input.stories.tsx
+++ src/shared/ui/Input/Input.stories.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { Input } from "./Input";
-import HideIcon from "@/shared/assets/icons/Hide.svg?react";
 import ShowIcon from "@/shared/assets/icons/Show.svg?react";
 
 const meta: Meta<typeof Input> = {
